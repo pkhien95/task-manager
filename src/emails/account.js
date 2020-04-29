11a1e3e8ce7d1926995fa94e9976e2ff9cfb1d0a
Hien Phung <pkhien95@gmail.com>
2020-04-29 11:03:23 +0700

    change activation link

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,7 +4,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendAccountConfirmationEmail = async (name, email, token) => {
 	try {
-		const activationLink = `http://localhost:3000/users/activate?accessToken=${token}`
+		const activationLink = `${process.env.API_URL}/users/activate?accessToken=${token}`
 		await sgMail.send({
 			from: 'pkhien95@gmail.com',
 			to: 'pkhien95@gmail.com',
